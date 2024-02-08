546109761964811bc69c65704495b28341d15417
KZapal <kamil.zapal@o2.pl>
2024-02-08 20:32:24 +0100

    components from goit materials

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,24 @@
+import { Route, Routes } from 'react-router-dom';
+import { Container, Header, Link } from './App.styled.js';
+import Home from '../pages/Home';
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
+    <div>
+      <Container>
+        <Header>
+          <nav>
+            <Link to="/" end>
+              Home
+            </Link>
+            <Link to="/movies">Movies</Link>
+          </nav>
+        </Header>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          {/* <Route path="/movies" element={} /> */}
+        </Routes>
+      </Container>
     </div>
   );
 };
